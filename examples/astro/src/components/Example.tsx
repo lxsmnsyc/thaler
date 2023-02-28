8c3dfef8dd5bcf968acde8e2d9c1f4fa3b112d87
Alexis Munsayac <alexis.munsayac@gmail.com>
2023-02-28 20:43:45 +0800

    Update Example.tsx

--- examples/astro/src/components/Example.tsx
+++ examples/astro/src/components/Example.tsx
@@ -1,12 +1,16 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { fn$ } from 'thaler';
 
 export default function Example(props: { message: string }) {
-  const logger = fn$((message) => {
-    console.log(`Message: ${message}`);
-    return null;
+  const logger = fn$((message: string) => {
+    const processed = `Message: ${message}`;
+    console.log(processed);
+    return processed;
   });
   return (
-    <button onClick={() => logger(props.message)}>
+    <button onClick={async () => {
+      console.log('Received', await logger(props.message));
+    }}>
       Click me!
     </button>
   );
