8432fa9296d39fba6c8593de1fb05149c41226b6
Alexis Munsayac <alexis.munsayac@gmail.com>
2023-03-30 15:15:47 +0800

    Fix type exports

--- packages/vite/src/index.ts
+++ packages/vite/src/index.ts
@@ -1,7 +1,7 @@
 import thalerUnplugin, { ThalerPluginOptions } from 'unplugin-thaler';
 import { Plugin } from 'vite';
 
-export { ThalerPluginFilter, ThalerPluginOptions } from 'unplugin-thaler';
+export type { ThalerPluginFilter, ThalerPluginOptions } from 'unplugin-thaler';
 
 const thalerPlugin = thalerUnplugin.vite as (options: ThalerPluginOptions) => Plugin;
 
