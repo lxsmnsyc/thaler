1da469343b7e3e8ec3d4eb9852bd8305aa2e86f5
Alexis Munsayac <alexis.munsayac@gmail.com>
2023-04-15 17:10:01 +0800

    Update server-count.ts

--- examples/sveltekit/src/components/server-count.ts
+++ examples/sveltekit/src/components/server-count.ts
@@ -1,4 +1,5 @@
 import { fn$ } from "thaler";
+import { debounce } from 'thaler/utils';
 
 const sleep = (ms: number) => new Promise((res) => {
   setTimeout(res, ms, true);
@@ -6,8 +7,10 @@ const sleep = (ms: number) => new Promise((res) => {
 
 const prefix = 'Server Count';
 
-export const serverCount = fn$(async (value: number) => {
+export const serverCount = debounce(fn$(async (value: number) => {
   await sleep(1000);
   console.log('Received', value);
   return `${prefix}: ${value}`;
+}), {
+  key: () => 'sleep',
 });
